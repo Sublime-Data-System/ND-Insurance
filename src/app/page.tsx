f9c1f1807d190815a460b223bfbd49a1caa5af34
priyanshusublime <priyanshu.sublime@gmail.com>
2025-11-27 09:10:55 +0530

    fix:homebg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="bg-[#ffffff]">
+    <div >
       <Hero />
       <WhyUs />
       <TestimonialsCarousel />
